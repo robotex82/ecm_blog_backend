39465bdfd91be2ce16cf63852cb8edb13380dafd
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-12-21 03:26:33 +0100

    Make posts positionable. Add filters to posts index. Truncate post body in index. Add missing dependecies.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Ecm::Blog::Backend::Engine.routes.draw do
   resources :posts do
     post :toggle_published, on: :member
+    post :reposition, on: :member
   end
   resources :asset_details do
     post :reposition, on: :member
